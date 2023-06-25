489fcc7223168a9d897c23bc6b2b964cee3e2b5f
Dikshantjung7 <133552875+Dikshantjung7@users.noreply.github.com>
2023-06-24 22:08:50 -0400

    Initial commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports =
+{
+    //"URI": "mongodb://127.0.0.1/book_store"
+    "URI": "mongodb+srv://dikshantkhadka2003:Kathmandu2003@assignment2.wkpda4o.mongodb.net/?retryWrites=true&w=majority"
+}
